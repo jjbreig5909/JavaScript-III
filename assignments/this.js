152ad5bd8a63606db0d97ec197fa0daedad10ff6
Jeff Breig <jjbreig@gmail.com>
2019-10-10 00:08:20 -0400

    answered first 4 principles

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
-* in your own words. explain the four principle for the "this" keyword below.
+* in your own words. explain the four principles for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window binding is when "this" refers to the Javascript global object itself. 
+* 2. Implicit binding is one of the most common uses for "this". It is used to refer to the object that a function lives inside of.
+* 3. New binding uses "this" as a generic placeholder so that you can use constructors to create new objects. Because it is generic it can take on the identity of whatever new object is specified. 
+* 4. Explicit binding is what occurs whenever using .call, .apply, or .bind on a function. In this useage it provides the context for all of the other parameters that go into the function. 
 *
 * write out a code example of each explanation above
 */
